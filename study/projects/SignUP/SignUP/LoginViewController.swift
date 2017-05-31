3148c2dde868420b2f3616b2f299b0f1d986b895
pcw <shine@pcw-MacBook-Pro.local>
2017-05-31 23:22:51 +0900

    20170531

--- study/projects/SignUP/SignUP/LoginViewController.swift
+++ study/projects/SignUP/SignUP/LoginViewController.swift
@@ -1,47 +1,109 @@
 //
-//  LoginViewController.swift
+//  ViewController.swift
 //  SignUP
 //
-//  Created by 샤인 on 2017. 5. 30..
+//  Created by 샤인 on 2017. 5. 28..
 //  Copyright © 2017년 IosCamp. All rights reserved.
 //
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController,UITextFieldDelegate {
     
     
-    var loginIdText = ""
-    let usersID:String = UserDefaults.standard.string(forKey: "NickName")!
-    @IBOutlet weak var loginWelcomeLb: UILabel!
+    
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var idNoticeLabel: UILabel!
+    @IBOutlet weak var passwordNoticeLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let userID = usersID
-        loginWelcomeLb.text = "\(userID)님 안녕하세요"
-     }
-
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    override func viewDidAppear(_ animated: Bool) {
-        let isAuthentified: Bool = UserDefaults.standard.bool(forKey: Authentification.authentificationBool)
-        super.viewDidAppear(animated)
-        if !isAuthentified {
-            self.performSegue(withIdentifier: "LoginViewSeg", sender: self)
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if idTextField.text == "" && passwordTextField.text == ""{
+            idTextField.text = ""
+            idTextField.becomeFirstResponder()
+        }else if passwordTextField.text == "" {
+            passwordTextField.text = ""
+            passwordTextField.becomeFirstResponder()
+        }else{
+            switch textField.tag {
+            case 1:
+                passwordTextField.becomeFirstResponder()
+            case 2:
+                passwordTextField.resignFirstResponder()
+            default:
+                textField.resignFirstResponder()
+            }
+        }
+        
+        return true
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+        loginRequest()
+        if idTextField.text == "" && passwordTextField.text == "" {
+            idNoticeLabel.text = "아이디를 확인해주세요"
+            passwordNoticeLabel.text = "비밀번호를 확인해주세요"
+        }else if idTextField.text == "" {
+            idNoticeLabel.text = "아이디를 확인해주세요"
+            passwordNoticeLabel.text = ""
+        }else if passwordTextField.text == "" {
+            passwordNoticeLabel.text = "비밀번호를 확인해주세요"
+            idNoticeLabel.text = ""
+        }else{
+            idNoticeLabel.text = ""
+            passwordNoticeLabel.text = ""
+            
         }
     }
-//    @IBAction func unwindToMain(_ segue:UIStoryboardSegue){
-//    }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        var loginCheck = true
+        if identifier == "loginSeg"{
+            if idTextField.text == "" || passwordTextField.text == "" {
+                loginCheck = false
+            }else{
+                loginCheck = true
+            }
+        }
+        return loginCheck
     }
-    */
-
+    
+    @IBAction func back(unwindSegue:UIStoryboardSegue){ //지금은 안쓰고 있고 그냥 세그 show로 연결해줬다
+        //back(함수명)이나 unwindSegue(파라미터)명은 상관없다 타입에 UIStoryboardSegue만 잘해주면 된다
+        //돌아갈뷰에 액션을 걸어준다 메인스토리보드에서 돌아가기전 뷰에서 돌아가는 버튼을 끌고 상단의 exit에 연결해주면 된다
+    }
+   
+    func loginRequest() {
+        if !(self.idTextField.text?.isEmpty)!
+            && !(self.passwordTextField.text?.isEmpty)!{
+            if self.idTextField.text! == UserDefaults.standard.string(forKey: Authentification.email) && self.passwordTextField.text! == UserDefaults.standard.string(forKey: Authentification.password){
+                print("로그인 성공")
+                UserDefaults.standard.set(true, forKey: Authentification.authentificationBool)
+            }else{
+                print("실패")
+            }
+        }
+    }
+    
+    //    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    //        let loginId:LoginViewController = segue.destination as! LoginViewController
+    //        loginId.loginIdText = idTextField.text!
+    //    }
+    
 }
+
---
+++ study/projects/SignUP/SignUP/MainViewController.swift
@@ -0,0 +1,63 @@
+//
+//  LoginViewController.swift
+//  SignUP
+//
+//  Created by 샤인 on 2017. 5. 30..
+//  Copyright © 2017년 IosCamp. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    
+    var loginIdText = ""
+    let usersID:String = UserDefaults.standard.string(forKey: "NickName")!
+    
+    
+    @IBOutlet weak var petImgView: UIImageView!
+    @IBOutlet weak var loginWelcomeLb: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let userID = usersID
+        loginWelcomeLb.text = "\(userID)님 안녕하세요"
+        
+        let petImages = [UIImage(named:"9.jpg")!,
+                         UIImage(named:"5.jpg")!,
+                         UIImage(named:"3.jpg")!,
+                         UIImage(named:"4.jpg")!,
+                         UIImage(named:"2.jpg")!,
+                         UIImage(named:"7.jpeg")!,
+                         UIImage(named:"8.jpeg")!,
+                         UIImage(named:"1.jpeg")!]
+        petImgView.animationImages = petImages
+        petImgView.animationDuration = 10.0
+        petImgView.startAnimating()
+     }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        let isAuthentified: Bool = UserDefaults.standard.bool(forKey: Authentification.authentificationBool)
+        super.viewDidAppear(animated)
+        if !isAuthentified {
+            self.performSegue(withIdentifier: "LoginViewSeg", sender: self)
+        }
+    }
+//    @IBAction func unwindToMain(_ segue:UIStoryboardSegue){
+//    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
