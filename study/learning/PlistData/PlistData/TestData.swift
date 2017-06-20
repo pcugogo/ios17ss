105f9880e8304b2f2cbfa96e21178686e4122a88
pcw <shine@pcw-MacBook-Pro.local>
2017-06-20 11:14:49 +0900

    20170620

--- study/learning/PlistData/PlistData/TestData.swift
+++ study/learning/PlistData/PlistData/TestData.swift
@@ -8,73 +8,36 @@
 
 import Foundation
 
-class TestData {
-    private static let instance:TestData = TestData()
-    
-    class var standard:TestData {
+class TestData{
+    private static var instance:TestData = TestData()
+    class var standard:TestData{
         return instance
     }
     private var dic:[String:Any]?
     private var realPath = ""
     
-    init() {
-        load()
-    }
-    deinit {
-        save()
-    }
-    
-    func object(forKey defaultName:String) -> Any? {
-        guard let data = dic else {
-            return nil
-        }
-        return data[defaultName]
-    }
-    
-    
-    func set(_ value: Any?, forKey defaultName: String){
-        guard let _ = dic, let dataTemp = value else{
-            return
-        }
-        return dic![defaultName] = dataTemp
-    }
-    
-    func removeObject(forKey defaultName:String) {
-        guard let _ = dic else {
-            return
-        }
-        dic!.removeValue(forKey: defaultName)
-        
-    }
-    
     
     func load(){
-        
-        let path:[String] = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
-        realPath = path[0] + "/TestList.plist"
+        let peth = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        realPath = peth[0] + "/testList.plist"
         
         let fileManager = FileManager.default
         
         if !fileManager.fileExists(atPath: realPath){
             if let bundlePath = Bundle.main.path(forResource: "TestList", ofType: "plist"){
-                
                 do {
                     try fileManager.copyItem(atPath: bundlePath, toPath: realPath)
                 } catch  {
-                    print("non defined error case")
                     return
                 }
             }
-            print("bundle no data")
-            return
+            
         }
-        dic = NSDictionary(contentsOfFile: realPath) as? [String : AnyObject]
+        dic = NSDictionary(contentsOfFile: realPath) as? [String : Any]
     }
     
     func save(){
         let nsDic = NSDictionary(dictionary: dic!)
-        
         nsDic.write(toFile: realPath, atomically: true)
     }
-    
 }
